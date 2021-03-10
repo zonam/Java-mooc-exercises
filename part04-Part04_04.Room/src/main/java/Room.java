c1ffb7bb2e32b7dff781ba6e85673c5d694c72a1
Manoj Timilsina <45329002+zonam@users.noreply.github.com>
2021-03-10 12:01:23 +0200

    Add files via upload

---
+++ part04-Part04_04.Room/src/main/java/Room.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author manojtimilsina
+ */
+public class Room {
+    private String code;
+    private int seats;
+    
+    public Room(String classcode, int numberOfSeats) {
+        this.code = classcode;
+        this.seats = numberOfSeats;
+}
+
+}
+
